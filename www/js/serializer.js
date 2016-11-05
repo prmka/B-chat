0dd62878e0c9ec408c98d641fc1ba0c764069988
Fun Maker <funmaker95@gmail.com>
2016-11-05 16:06:08 +0100

    Basic project structure part 3, final

--- www/js/serializer.js
+++ www/js/serializer.js
@@ -3,17 +3,11 @@ angular.module('starter.serializer', [])
 .factory('Serializer', function() {
 
 	return {
-        // Stara sie utworzyć jeden pakiet z danych z buforu
-        // Zwraca `pakiet, resztaDanych` jak sie uda
-        // jak buffer jest pusty albo nie ma w nim calego pakiety to zwraca `null`
-        // buffer i resztaDanych to https://nodejs.org/api/buffer.html
-        // resztaDanych powinna zostac zapisana bo moze zawierac kolejne pakiety
 		fromBytes: function(buffer) {
-            
+            JSON.stringify(buffer.toString('utf8'))
 		},
-        // serializuje pakiet i zwraca buffer
 		toBytes: function(packet) {
-            
+            JSON.stringify(packet)
 		}
 	};
 })
