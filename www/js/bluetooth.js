9983f50c28248904af5692c0b055f9b0bf8f329d
shead <sheadovas@gmail.com>
2016-12-12 22:03:36 +0100

    bluetooth.js: modified docs comments

--- www/js/bluetooth.js
+++ www/js/bluetooth.js
@@ -5,11 +5,11 @@ angular.module('starter.bluetooth', ['ionic'])
 
 	// struct DevInfo
 	// 
-	// Struktura opisuje dost?pne informacje o urz?dzeniu
+	// Struktura opisuje dostepne informacje o urzadzeniu
 	// FIELDS
-	//   id                      ID urz?dzenia
-	//   name                    Wy?wietlana nazwa urz?dzenia
-	//   paired                  Informacja o aktualnym stanie sparowania urz?dzenia
+	//   id                      ID urzadzenia
+	//   name                    Wyswietlana nazwa urzadzenia
+	//   paired                  Informacja o aktualnym stanie sparowania urzadzenia
 	/* struct DevInfo {
 	     int id;
 	     string name;
@@ -20,7 +20,7 @@ angular.module('starter.bluetooth', ['ionic'])
 	return {
 		// void refreshList()
 		//
-		// Aktualizuje list? dost?pnych urz?dze?
+		// Aktualizuje liste dostepnych urzadzen
 		refreshList: function () {
 
 			// HACK
@@ -36,8 +36,8 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// {(int, string, bool)} list()
 		//
-		// Zwraca list? wszystkich aktualnie dost?pnych urz?dze?,
-		// zar�wno tych po??czonych jak i tylko dost?pnych
+		// Zwraca liste wszystkich aktualnie dostepnych urzadzen,
+		// zarowno tych polaczonych jak i tylko dostepnych
 		//
 		// RETURNS
 		//   {(id, name, paired)}    Kolekcja list o polach: id, nazwa urzadzenia, czy sparowane
@@ -49,10 +49,10 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// void setName(string)
 		// 
-		// Zmienia wy?wietlan? nazw? urz?dzenia
+		// Zmienia wyswietlana nazwe urzadzenia
 		//
 		// PARAMS
-		//   name                     Nowa wy?wietlana nazwa urz?dzenia
+		//   name                     Nowa wyswietlana nazwa urzadzenia
         setName: function(name) {
             // TODO shd
         },
@@ -60,11 +60,11 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// bool isConnected()
 		//
-		// Sprawdza czy urz?dzenie jest po??czone z chatem
+		// Sprawdza czy urzadzenie jest polaczone z chatem
 		//
 		// RETURNS
-		//   true                      Uda?o si? nawi?za? po??czenie
-		//   false                     Urz?dzenie nie jest po??czone
+		//   true                      Udalo sie nawiazac polaczenie
+		//   false                     Urzadzenie nie jest polaczone
         isConnected: function() {
             // TODO shd
         },
@@ -72,10 +72,10 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// void connect(int)
 		//
-		// ??czy z urz?dzeniem o podanym id
+		// polacz z urzadzeniem o podanym id
 		//
 		// PARAMS
-		//   id                         ID urz?dzenia, z kt�rym chcemy si? po??czy?
+		//   id                         ID urzadzenia, z ktrrym chcemy sie polaczyc
         connect: function(id) {
             // TODO shd
         },
@@ -83,11 +83,11 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// bool isVisible()
 		//
-		// Sprawdza czy urz?dzenie jest widoczne dla niesparowanych urz?dze?
+		// Sprawdza czy urzadzenie jest widoczne dla niesparowanych urzadzen
 		//
 		// RETURNS
-		//   true                       Urz?dzenie jest widoczne
-		//   false                      Urz?dzenie nie jest widoczne
+		//   true                       Urzadzenie jest widoczne
+		//   false                      Urzadzenie nie jest widoczne
         isVisible: function() {
             // TODO shd
         },
@@ -95,10 +95,10 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// void setVisible(bool)
 		//
-		// Ustawia widoczno?? urz?dzenia dla niesparowanych urz?dze?
+		// Ustawia widocznosc urzadzenia dla niesparowanych urzadzen
 		//
 		// PARAMS
-		//   visibility                  Widoczno?? urz?dzenie (true = widoczny, false = niewidoczny)
+		//   visibility                  Widocznosc urzadzenie (true = widoczny, false = niewidoczny)
         setVisible: function(visibility) {
             // TODO shd
         },
@@ -106,11 +106,11 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// bool isSearching()
 		//
-		// Sprawdza czy auto-wyszukiwanie nowych urz?dze? jest w??czone
+		// Sprawdza czy auto-wyszukiwanie nowych urzadzen jest wlaczone
 		// 
 		// RETURNS
-		//   true                        Wykonywany jest auto-update dost?pnych urz?dze?
-		//   false                       Dost?pne urz?dzenia nie s? aktualizowane
+		//   true                        Wykonywany jest auto-update dostepnych urzadzen
+		//   false                       Dostepne urzadzenia nie sa automatycznie aktualizowane
         isSearching: function () {
             // TODO shd
         },
@@ -118,10 +118,10 @@ angular.module('starter.bluetooth', ['ionic'])
 
 		// void setSearching()
 		//
-		// Ustawia auto-wyszukiwanie nowych urz?dze?, z kt�rymi mo?na si? po??czy?
+		// Ustawia auto-wyszukiwanie nowych urzadzen, z ktorymi mozna sie polaczyc
 		// 
 		// PARAMS
-		//   searching                    Ustawia auto-wyszykiwanie (true = w??czone, false = wy??czone)
+		//   searching                    Ustawia auto-wyszykiwanie (true = wlaczone, false = wylaczone)
         setSearching: function(searching) {
             // TODO shd
         }
