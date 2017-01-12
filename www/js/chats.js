6e1e080b99f2de37bc2e0f704947f96b50f681f4
prmka <przemyslaw.wdowczyk@gmail.com>
2017-01-12 12:37:58 +0100

    E
    historia + male poprawki

--- www/js/chats.js
+++ www/js/chats.js
@@ -1,55 +1,67 @@
 angular.module('starter.chats', [])
 
-.factory('Chats', function(Bluetooth, $rootScope) {
+.factory('Chats', function(Bluetooth, $rootScope, $state) {
 	
-	var chats = []
-	var currentChat = { chat: [] }
-	
-	function updateChats() {
-		var devices = Bluetooth.list();
-		chats = []
-		for(addr in devices){
-			var device = devices[addr]
-			chats.push({
-				name: device.name,
-				address: device.address,
-				paired: device.paired,
-				chat: [],
-			})
-		}
+	if( localStorage.getItem('history') == undefined) {
+		localStorage.setItem('history', '{}')
 	}
 	
+	var history = JSON.parse(localStorage.getItem('history'))
+	var currentChat = { chat: [] }
+	
 	Bluetooth.onMessage(message => {
-		currentChat.chat.push({owner: "his", text: message.text})
-		$rootScope.$broadcast("newMessage");
+		switch(message.type){
+			case "text":
+				currentChat.chat.push({owner: "his", text: message.text})
+				localStorage.setItem('history', JSON.stringify(history))
+				$rootScope.$broadcast("newMessage");
+			break;
+			case "handshake":
+				chats = Bluetooth.list();
+				for(addr in chats){
+					if(chats[addr].name == message.name){
+						$state.go("tab.chat", {"chatId": addr})
+						return
+					}
+				}
+				alert("Failed to start chat!\nRefresh device list and try again.")
+				Bluetooth.disconnect()
+			break;
+		}
 	})
 
 	return {
         // zwraca liste wszystkich czatow
 		list: function() {
-			updateChats()
-			return chats;
+			return Bluetooth.list();
+		},
+        // rozpoczyna czat
+		startChat: function(addr) {
+			Bluetooth.connect(addr)
 		},
         // zwraca konkretny czat
-		startChat: function(addr) {
-			updateChats()
-			for(chat of chats) {
-				if(chat.address == addr){
-					Bluetooth.connect(addr)
-					currentChat = chat
-					return chat;
-				}
-			}
-			currentChat = chats[0]
-			return chats[0];
+		getChat: function(addr) {
+			chats = Bluetooth.list();
+			currentChat = chats[addr]
+			if(currentChat == undefined) currentChat = {}
+			if(history[addr] == undefined) history[addr] = []
+			currentChat.chat = history[addr]
+			return chats[addr];
 		},
         // zostaje odpalony kiedy ma zostac wyslana wiadomosc do rozmowcy
         onInput: function(text) {
+			if(text == "") return
             Bluetooth.send({
 				type: "text",
 				text: text,
 			})
 			currentChat.chat.push({owner: "your", text: text})
+			localStorage.setItem('history', JSON.stringify(history))
+        },
+        // usuwa historie
+        clearHistory: function(text) {
+			history = {}
+			localStorage.setItem('history', JSON.stringify(history))
         }
 	};
 });
